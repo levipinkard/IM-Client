f2ac7119da046f47ece6366bb0c7ae91d22b5044
Levi Pinkard <levipinkard@gmail.com>
2016-12-21 13:17:25 -0500

    Fixed everything

--- IMServer.java
+++ IMServer.java
@@ -1,3 +1,4 @@
+//Levi Pinkard, 2016
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -11,48 +12,57 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.util.Scanner;
+import java.net.InetAddress;
 
 
 public class IMServer extends Thread {
-  PrintWriter writer;
 	static class ServerThread implements Runnable {
-    static String oldString;
-    static String newString;
-    static String[] chatStr = new String[2];
-		Socket client = null;
-    static int threadCount = 0;
+    //Declares variable for whole class
+    Socket client = null;
 		public ServerThread(Socket c) {
       client = c;
     }
-		public void run() {
-      int localThreadCount = threadCount;
-      threadCount = threadCount + 1;
+    //Thread running in background recieves messages
+    public void run() {
       while(true) {
 				try {
-          //BufferedReader clientData = new BufferedReader(new InputStreamReader(client.getInputStream()));
-          DataOutputStream backToClient = new DataOutputStream(client.getOutputStream());
-          //String clientMessage = clientData.readLine();
-          //string = clientMessage
-					backToClient.writeBytes("what up" + "\n");
-
+          //Gets input stream from client
+          BufferedReader clientData = new BufferedReader(new InputStreamReader(client.getInputStream()));
+          //Sets clientMessage to text sent by client
+          String clientMessage = clientData.readLine();
+          //Stops from spamming null at disconnect
+          if (clientMessage != null) {
+            System.out.println(clientMessage);
+          }
 				}catch (Exception e) {
 					System.err.println(e.getMessage());
 				}
+
 			}
 		}
 	}
 	public static void main(String[] args) throws Exception {
-		String clientName;
-		try{
-		    PrintWriter writer = new PrintWriter("chatlog.txt", "UTF-8");
-		    writer.close();
-		} catch (IOException e) {
-		   // do something
-		}
-		ServerSocket serverSock = new ServerSocket(1337);
-		while(true) {
-			Socket connection = serverSock.accept();
+    //Gets username
+    System.out.print("Enter username: ");
+    BufferedReader userName = new BufferedReader(new InputStreamReader(System.in));
+    String userString = userName.readLine();
+    String clientName;
+    //Prints hostname of system for easy connection
+    System.out.println("Hostname: " + InetAddress.getLocalHost().getHostName());
+    //Opens socket at port 1337
+    ServerSocket serverSock = new ServerSocket(1337);
+    //Accepts connection from client
+  	Socket connection = serverSock.accept();
+    //Loops infinitely
+    while(true) {
+      //Starts thread
       new Thread(new ServerThread(connection)).start();
+      //Gets user text, sends it
+      DataOutputStream backToClient = new DataOutputStream(connection.getOutputStream());
+      BufferedReader textIn = new BufferedReader(new InputStreamReader(System.in));
+      String thisClient = textIn.readLine();
+      //Attaches username when sending
+      backToClient.writeBytes(userString + ": " + thisClient + "\n");
 		}
 	}
 }
--- imclient.java
+++ imclient.java
@@ -1,3 +1,4 @@
+//Levi Pinkard, 2016
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -11,19 +12,26 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.util.Scanner;
+import java.net.InetAddress;
 
 public class imclient extends Thread {
 	static class ServerThread implements Runnable {
+		//Declares client for whole class
 		Socket client = null;
 		public ServerThread(Socket c) {
 			client = c;
 		}
+		//Thread that runs in background, recieving messages
 		public void run() {
 			while (true) {
 				try {
+					//Gets incoming message, prints it
 					BufferedReader incomingMessage = new BufferedReader(new InputStreamReader(client.getInputStream()));
 					String incoming = incomingMessage.readLine();
-					System.out.println(incoming);
+					//Stops null spamming at disconnect
+					if (incoming != null) {
+						System.out.println(incoming);
+					}
 				}
 				catch (IOException e) {
 					System.err.println(e.getMessage());
@@ -32,22 +40,26 @@ public class imclient extends Thread {
 		}
 	}
 	public static void main(String[] args) throws Exception {
+		//Gets username
 		System.out.print("Enter username: ");
 		Scanner nameIn = new Scanner(System.in);
 		String userName = nameIn.nextLine();
+		//Gets hostname for connection
 		System.out.print("Enter hostname: ");
 		Scanner hostIn = new Scanner(System.in);
 		String hostName = hostIn.nextLine();
 		String message = null;
+		//Starts socket at user specified hostname
 		Socket server = new Socket(hostName, 1337);
 		new Thread(new ServerThread(server)).start();
+		//Loops infinitely
 		while (true) {
+			//Gets user message, opens output stream
 			BufferedReader userMessage = new BufferedReader(new InputStreamReader(System.in));
 			message = userMessage.readLine();
 			DataOutputStream broadcastMessage = new DataOutputStream(server.getOutputStream());
+			//Attaches username to message when sending
 			broadcastMessage.writeBytes(userName + ": " + message + "\n");
 		}
-		//hostIn.close();
-		//server.close();
 	}
 }
